e0fd5b1f6622fb21eb4d2100e9e17395c7384e2d
huanghanzhilian <1319639755@qq.com>
2017-11-04 03:54:03 +0800

    update code signup and signin

--- app.js
+++ app.js
@@ -45,10 +45,88 @@ app.set('views','./views/pages');       // 设置视图默认的文件路径
 app.set('view engine','jade');          // 设置视图引擎：jade
 
 var Movie=require('./models/movie.js');  // 载入mongoose编译后的模型movie
-
+var User=require('./models/user.js');  // 载入mongoose编译后的模型user
 
 
 // 编写主要页面路由
+
+//signup
+//post参数第一个路由，第二个回掉方法
+//回掉方法参数request请求
+//回掉方法参数response响应体
+app.post('/user/signup',function(req,res){
+    //获取表单数据
+    var _user=req.body.user;
+    //查找输入用户名是否已存在
+    User.findOne({name:_user.name},function(err,user){
+        if(err){
+            console.log(err)
+        }
+        if(user){
+            return res.redirect('/');
+        }else{
+            //生成用户数据
+            var user=new User(_user);
+            //保存数据方法
+            user.save(function(err,user){
+                if (err) {
+                    console.log(err)
+                }
+                //重定向到首页
+                res.redirect('/admin/userlist');
+            })
+        }
+    })
+})
+
+// userlist 列表页
+app.get('/admin/userlist', function (req, res) {
+    User.fetch(function (err, users) {
+        if (err) {
+            console.log(err);
+        }
+        res.render('userlist', {//视图views
+            title: '用户列表页',
+            users: users
+        });
+    });
+});
+
+//signin
+app.post('/user/signin',function(req,res){
+    //拿到表单提交的user
+    var _user=req.body.user;
+    var name=_user.name;
+    var password=_user.password;
+    //查询数据库是否有该用户
+    User.findOne({name:name},function(err,user){
+        if(err){
+            console.log(err)
+        }
+        if(!user){
+            return res.redirect('/');
+        }
+        //匹配密码
+        //用户提交的密码是明文的数据库是加密后的密码所以这个时候需要调用这个user实例上的方法
+        //传入当前密码，拿到一个回掉的方法
+        //isMatch通过他看是否匹配正确
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) {
+                console.log(err)
+            }
+            if (isMatch) {
+                //req.session.user = user
+                return res.redirect('/')
+            } else {
+                console.log('不匹配')
+                //return res.redirect('/signin')
+            }
+        })
+    })
+})
+
+
+
 // index page 首页
 app.get('/', function (req, res)  {
     Movie.fetch(function (err, movies) {
--- schemas/user.js
+++ schemas/user.js
@@ -1,16 +1,17 @@
 //引入mongoose建模工具模块
 var mongoose=require('mongoose');
 
+//用来密码存储设计的一个方法
+var bcrypt =require('bcrypt');
+var SALT_WORK_FACTOR=10;
+
 //声明一个MovieSchema调用mongoose的Schema方法传入对象
 var UserSchema=new mongoose.Schema({
 	name:{
 		unique:true,
 		type:String
 	},
-	password:{
-		unique:true,
-		type:String
-	},
+	password:String,
 	meta:{//录入数据或更新时，时间记录
 		createAt:{//创建时间
 			type:Date,//日期类型
@@ -24,17 +25,50 @@ var UserSchema=new mongoose.Schema({
 });
 
 
+
 //为模式添加一个方法
 //每次添加数据之前都会来调用这个方法
 UserSchema.pre('save',function(next){
+	var user =this;
 	//判断数据是否是新加的
 	if(this.isNew){
 		this.meta.createAt=this.meta.updataAt=Date.now();
 	}else{
 		this.meta.updataAt=Date.now();
 	}
-	next();
+
+	bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+		if(err) return next(err)
+		bcrypt.hash(user.password,salt,function(err,hast){
+			if(err) return next(err)
+			user.password=hast;
+			next();
+		})
+	})
+
+	//next();
 });
+
+UserSchema.methods = {
+	//增加一个comparePassword实例方法
+	//接收参数1 用户提交过来的密码
+	//接收参数2 接收一个回掉方法
+	comparePassword: function(_password, cb) {
+		//同样bcrypt.compare做密码校对
+		//参数1明文密码
+		//2数据库密码
+		//3回掉函数
+		//参数1错误信息
+		//参数2 isMatch是否正确
+		bcrypt.compare(_password, this.password, function(err, isMatch) {
+			//如果有错误的话直接把错误暴露给回掉方法里返回
+			if (err) return cb(err)
+			//没有错的话将错误设置为null
+			//将isMatch匹配的状态返回
+			cb(null, isMatch)
+		})
+	}
+}
 
 
 // movieSchema 模式的静态方法
