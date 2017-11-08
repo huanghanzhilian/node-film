2e8d5f78a3809415d60a2ff5689427df95fc9c38
huanghanzhilian <1319639755@qq.com>
2017-11-09 00:44:34 +0800

    update code paging

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -35,3 +35,46 @@ exports.index = function(req, res) {
 		});
 	});*/
 }
+
+// search page
+exports.search=function(req,res){
+	//拿到分类id
+	var catId=req.query.cat;
+	//拿到页码
+	var page = parseInt(req.query.p, 10) || 0;
+	var count = 2
+	//每一页只展示两条数据
+	var index = page * count
+
+	Category
+		.find({
+			_id: catId
+		})
+		.populate({
+			path: 'movies',
+			select: 'title poster',
+			/*options: {
+				limit: 2,
+				skip:index
+			}*/
+		})
+		.exec(function(err, categories) {
+			if (err) {
+				console.log(err)
+			}
+			var category = categories[0] || {}
+	        var movies = category.movies || []
+	        var results = movies.slice(index, index + count)
+
+			res.render('results', {
+				title: 'imooc 结果列表页面',
+				keyword: category.name,
+				currentPage: (page + 1),
+				query: 'cat=' + catId,
+				totalPage: Math.ceil(movies.length / count),
+				movies: results
+			})
+		})
+}
+
+
